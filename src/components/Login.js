8fc5a5f3e68e03d1d3938a7cda94ffb72706ae3f
hellodajana <dajanasmolyakov@gmail.com>
2023-01-05 19:05:22 +0100

    Add form to Login component

---
+++ src/components/Login.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const Login = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    const newData = { ...data };
+    newData[e.target.id] = e.target.value;
+    setData(newData);
+    console.log(newData);
+  };
+
+  return (
+    <section className="login">
+      <h1 className="login__title">Login</h1>
+      <form className="login__form" onSubmit={handleSubmit}>
+        <label className="login__label" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="login__input"
+          type="email"
+          id="email"
+          name="email"
+          required
+          value={data.email}
+          onChange={(e) => handleChange(e)}
+        />
+        <label className="login__label" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="login__input"
+          type="password"
+          id="password"
+          name="password"
+          autoComplete="off"
+          value={data.password}
+          onChange={(e) => handleChange(e)}
+        />
+        <button className="btn">Login</button>
+        <p className="login__error"></p>
+      </form>
+    </section>
+  );
+};
+
+export default Login;
