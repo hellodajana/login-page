37805e891c96a04c017c7b5159196a30275d63f8
hellodajana <dajanasmolyakov@gmail.com>
2023-01-08 19:29:02 +0100

    Add routes with react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.scss";
 import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
